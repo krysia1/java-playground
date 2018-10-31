fa634835b4e7ec11fb59e23125c7207b82ee3c8d
Krysia <krysia@agabox.net>
2018-10-31 13:12:52 +0100

    first commit

---
+++ src/com/company/TShirtProperties.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public enum TShirtProperties {
+    S,
+    M,
+    L,
+    XL;
+
+    private TShirtProperties size;
+    private String manufacturer;
+
+    TShirtProperties(TShirtProperties size,String manufacturer){
+        this.size=size;
+        this.manufacturer=manufacturer;
+
+    }
+
+}
